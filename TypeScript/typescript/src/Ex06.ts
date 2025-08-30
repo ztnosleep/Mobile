a6a1db647a81973975edf296fbb5f9ade7ed496a
zt <153701963+Thai0Zui@users.noreply.github.com>
2025-08-30 12:00:46 +0700

    commit bai6

---
+++ TypeScript/typescript/src/Ex06.ts
@@ -0,0 +1,10 @@
+class Book{
+    title: String;
+    author: String;
+    year:  number;
+    constructor(title: String, author: String, year: number){
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+}
