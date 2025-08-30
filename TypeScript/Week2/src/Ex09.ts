ffd42e0fea4bb2c6be7915a2e78c4307a1bb51c9
zt <153701963+Thai0Zui@users.noreply.github.com>
2025-08-30 14:32:20 +0700

    commit bai9 tuan2, mc9

---
+++ TypeScript/Week2/src/Ex09.ts
@@ -0,0 +1,19 @@
+function filterEvenNumbers(array: number[]): Promise<number[]> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const evenNumbers = array.filter((num) => num % 2 === 0);
+        resolve(evenNumbers);
+      }, 1000); 
+    });
+  }
+  
+
+  const numbers = [1, 2, 3, 4, 5, 6];
+  
+  filterEvenNumbers(numbers)
+    .then((result) => {
+      console.log("Even numbers:", result); 
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
