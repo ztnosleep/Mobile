e3f88190cf0f04984f3e1e0f041df9bda72b7faf
zt <153701963+Thai0Zui@users.noreply.github.com>
2025-08-30 13:25:31 +0700

    commit bai2 tuan2

---
+++ TypeScript/Week2/dist/Ex02.js
@@ -0,0 +1,11 @@
+"use strict";
+function resolveAfterOneSecond() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(10);
+        }, 1000);
+    });
+}
+resolveAfterOneSecond().then((value) => {
+    console.log(value);
+});
