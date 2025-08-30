b77c37458cfb8c9e75155ad2046225434a84bbe6
zt <153701963+Thai0Zui@users.noreply.github.com>
2025-08-30 14:13:05 +0700

    commit bai8 tuan2, mc8

---
+++ TypeScript/Week2/dist/Ex08.js
@@ -0,0 +1,20 @@
+"use strict";
+Promise.resolve(2)
+    .then((num) => {
+    console.log(`Starting with: ${num}`);
+    return num * num;
+})
+    .then((squared) => {
+    console.log(`Squared: ${squared}`);
+    return squared * 2;
+})
+    .then((doubled) => {
+    console.log(`Doubled: ${doubled}`);
+    return doubled + 5;
+})
+    .then((finalResult) => {
+    console.log(`Final result: ${finalResult}`);
+})
+    .catch((err) => {
+    console.error("Something went wrong:", err);
+});
